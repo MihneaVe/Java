3421d6d5bf9c22e3972ac1172f23e98ac29ac2dd
MihneaVe <mihneave2004@gmail.com>
2025-03-14 13:16:13 +0200

    Tema_2_grades_rounder

--- Tema_Lab_2/src/Classroom/Classroom/Main.java
+++ Tema_Lab_2/src/Classroom/Classroom/Main.java
@@ -5,12 +5,12 @@ public class Main {
         Student[] students = new Student[6];
 
         // Create 6 students (could be made with a loop and reading from a file but for the sake of simplicity they are hardcoded) 
-        students[0] = new Student("John", 20, new Adresa("Strada1", 1, "Oras1", "Tara1"), 1, 1, new int[]{1, 1, 7, 8});
-        students[1] = new Student("Jane", 21, new Adresa("Strada2", 2, "Oras2", "Tara2"), 2, 2, new int[]{6, 7, 8, 9});
-        students[2] = new Student("Jack", 22, new Adresa("Strada3", 3, "Oras3", "Tara3"), 3, 3, new int[]{7, 8, 9, 10});
-        students[3] = new Student("Jill", 23, new Adresa("Strada4", 4, "Oras4", "Tara4"), 4, 4, new int[]{8, 9, 10, 10});
-        students[4] = new Student("Jim", 24, new Adresa("Strada5", 5, "Oras5", "Tara5"), 5, 5, new int[]{9, 10, 10, 10});
-        students[5] = new Student("Jill2", 25, new Adresa("Strada6", 6, "Oras6", "Tara6"), 6, 6, new int[]{10, 10, 10, 10});
+        students[0] = new Student("John", 20, new Adresa("Strada1", 1, "Oras1", "Tara1"), 1, 1, new float[][]{{1,2,5,4}, {1,5,3}, {7,9,3}, {8,9,10}});
+        students[1] = new Student("Jane", 21, new Adresa("Strada2", 2, "Oras2", "Tara2"), 2, 2, new float[][]{{6,4}, {7,8}, {8,10,8}, {9,10}});
+        students[2] = new Student("Jack", 22, new Adresa("Strada3", 3, "Oras3", "Tara3"), 3, 3, new float[][]{{7,6}, {8,9,10}, {9,10,7}, {10,8,2}});
+        students[3] = new Student("Jill", 23, new Adresa("Strada4", 4, "Oras4", "Tara4"), 4, 4, new float[][]{{8, 7}, {9, 8}, {10, 9}, {10, 9}});
+        students[4] = new Student("Jim", 24, new Adresa("Strada5", 5, "Oras5", "Tara5"), 5, 5, new float[][]{{9, 8}, {10, 9}, {10, 9}, {10, 9}});
+        students[5] = new Student("Jill2", 25, new Adresa("Strada6", 6, "Oras6", "Tara6"), 6, 6, new float[][]{{10, 9}, {10, 9}, {10, 9}, {10, 9}});
 
         // Print the situation of each student
         for(int i=0; i<6; i++){
--- Tema_Lab_2/src/Classroom/Classroom/Student.java
+++ Tema_Lab_2/src/Classroom/Classroom/Student.java
@@ -6,10 +6,10 @@ import java.util.Comparator;
 public final class Student extends Person {
     private int studyYear;
     private int studyGroup;
-    private int[] gradesPerDiscipline = new int[4];
+    private float[][] gradesPerDiscipline = new float[4][];
 
     // Constructor
-    public Student(String string, int i, Adresa adresa, int j, int k, int[] ls) {
+    public Student(String string, int i, Adresa adresa, int j, int k, float[][] ls) {
         super(string, i, adresa);
         this.studyYear = j;
         this.studyGroup = k;
@@ -17,22 +17,36 @@ public final class Student extends Person {
     }
 
     // Getters from README.md
-    public int getYearAverage() {
-        int sum = 0;
+    public float getYearAverage() {
+        float sum = 0;
         for (int i = 0; i < 4; i++) {
-            sum += gradesPerDiscipline[i];
+            float media = 0;
+            for (int j=0; j<gradesPerDiscipline[i].length; j++){
+                media += this.gradesPerDiscipline[i][j];
+            }
+            media /= gradesPerDiscipline[i].length;
+            sum += media;
         }
         return sum / 4;
-    }
-
-    public void getSituationship(){
-        System.out.println("The student " + this.getName() + " part of class "+ this.studyGroup + " has an average of " + this.getYearAverage() + " and is in year " + this.studyYear + " of study.");
-        if (this.getYearAverage() >= 5) {
-            System.out.println("The student " + this.getName() + " has passed the year.\n");
-        } else {
-            System.out.println("The student " + this.getName() + " has NOT passed the year.\n");
         }
-    }
+
+        public void getSituationship() {
+            System.out.println("The student " + this.getName() + " part of class " + this.studyGroup + " has an average of " + Math.round(this.getYearAverage()) + " and is in year " + this.studyYear + " of study.");
+        
+            boolean hasEnoughGrades = true;
+            for (int i = 0; i < 4; i++) {
+                if (this.gradesPerDiscipline[i].length < 2) {
+                hasEnoughGrades = false;
+                break;
+                }
+            }
+            
+            if (this.getYearAverage() >= 5 && hasEnoughGrades) {
+                System.out.println("The student " + this.getName() + " has passed the year.\n");
+            } else {
+                System.out.println("The student " + this.getName() + " has NOT passed the year.\n");
+            }
+        }
 
     // Method to print the top 3 students
     public static void Top3Students(Student[] students) {
@@ -40,14 +54,14 @@ public final class Student extends Person {
         Arrays.sort(students, new Comparator<Student>() {
             @Override
             public int compare(Student s1, Student s2) {
-                return Integer.compare(s2.getYearAverage(), s1.getYearAverage());
+                return Float.compare(s2.getYearAverage(), s1.getYearAverage());
             }
         });
 
         // Print the top 3 students
         System.out.println("Top 3 students:");
         for (int i = 0; i < 3 && i < students.length; i++) {
-            System.out.println((i + 1) + ". " + students[i].getName() + " with an average of " + students[i].getYearAverage());
+            System.out.println((i + 1) + ". " + students[i].getName() + " with an average of " + Math.round(students[i].getYearAverage()));
         }
     }
 
